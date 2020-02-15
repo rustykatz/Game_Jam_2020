01227eae2bd0b88f293bf407e00f3bc3c812b77a
JimRanda <jimmymranda@gmail.com>
2020-02-14 22:41:33 -0800

    Create Player.cs

---
+++ Assets/Scripts/Jimmys Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float rotation;
+    public float rotationY;
+    public Vector3 velocity;
+    public float gravity = -9.8f;
+    public CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+
+        rotation -= Input.GetAxis("Mouse Y");
+        rotationY = transform.Find("Main Camera").localEulerAngles.y;
+        transform.Find("Main Camera").localEulerAngles = new Vector3(rotation, rotationY, 0);
+
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(move * Time.deltaTime);
+
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        if(controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
